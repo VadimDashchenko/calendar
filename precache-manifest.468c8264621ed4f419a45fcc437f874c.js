a55242d4ecf1140216b71d7c2a66222d2886192c
VadimDashchenko <dashchenko.vad.92@gmail.com>
2020-06-09 12:19:48 +0300

    Updates

---
+++ precache-manifest.468c8264621ed4f419a45fcc437f874c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5dca765b877240b7415647e328c838",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "8b1713d1a8c39371820e",
+    "url": "/calendar/static/css/2.b882b93e.chunk.css"
+  },
+  {
+    "revision": "e6d968f13fe73050d3db",
+    "url": "/calendar/static/css/main.a00b7177.chunk.css"
+  },
+  {
+    "revision": "8b1713d1a8c39371820e",
+    "url": "/calendar/static/js/2.52e05aec.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/calendar/static/js/2.52e05aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d968f13fe73050d3db",
+    "url": "/calendar/static/js/main.f28e55b3.chunk.js"
+  },
+  {
+    "revision": "0d5c1feacd1219e7629f",
+    "url": "/calendar/static/js/runtime-main.ad1f5134.js"
+  }
+]);
